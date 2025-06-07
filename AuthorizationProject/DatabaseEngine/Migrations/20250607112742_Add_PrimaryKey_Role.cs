13a8e42ca0d0fec406200255caf46f560f0cb872
alexy.alexy98@mail.ru <alexy.alexy98@mail.ru>
2025-06-07 14:37:29 +0300

    Добавил миграции. Применил в БД

---
+++ AuthorizationProject/DatabaseEngine/Migrations/20250607112742_Add_PrimaryKey_Role.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_PrimaryKey_Role : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RoleCode",
+                schema: "dbo",
+                table: "UserRole");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRole_RoleCode",
+                schema: "dbo",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                schema: "dict",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "RoleCode",
+                schema: "dbo",
+                table: "UserRole");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleCode",
+                schema: "dict",
+                table: "Role",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "dict",
+                table: "Role",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                schema: "dict",
+                table: "Role",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_RoleId",
+                schema: "dbo",
+                table: "UserRole",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                schema: "dbo",
+                table: "UserRole",
+                column: "RoleId",
+                principalSchema: "dict",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                schema: "dbo",
+                table: "UserRole");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRole_RoleId",
+                schema: "dbo",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                schema: "dict",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "dict",
+                table: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleCode",
+                schema: "dbo",
+                table: "UserRole",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleCode",
+                schema: "dict",
+                table: "Role",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                schema: "dict",
+                table: "Role",
+                column: "RoleCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_RoleCode",
+                schema: "dbo",
+                table: "UserRole",
+                column: "RoleCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RoleCode",
+                schema: "dbo",
+                table: "UserRole",
+                column: "RoleCode",
+                principalSchema: "dict",
+                principalTable: "Role",
+                principalColumn: "RoleCode",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ AuthorizationProject/Patches/Patch_20250607_Add_PrimaryKey_Role.sql
@@ -0,0 +1,37 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [dbo].[UserRole] DROP CONSTRAINT [FK_UserRole_Role_RoleCode];
+
+DROP INDEX [IX_UserRole_RoleCode] ON [dbo].[UserRole];
+
+ALTER TABLE [dict].[Role] DROP CONSTRAINT [PK_Role];
+
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[dbo].[UserRole]') AND [c].[name] = N'RoleCode');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [dbo].[UserRole] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [dbo].[UserRole] DROP COLUMN [RoleCode];
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[dict].[Role]') AND [c].[name] = N'RoleCode');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [dict].[Role] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [dict].[Role] ALTER COLUMN [RoleCode] nvarchar(max) NOT NULL;
+
+ALTER TABLE [dict].[Role] ADD [Id] int NOT NULL IDENTITY;
+
+ALTER TABLE [dict].[Role] ADD CONSTRAINT [PK_Role] PRIMARY KEY ([Id]);
+
+CREATE INDEX [IX_UserRole_RoleId] ON [dbo].[UserRole] ([RoleId]);
+
+ALTER TABLE [dbo].[UserRole] ADD CONSTRAINT [FK_UserRole_Role_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dict].[Role] ([Id]) ON DELETE CASCADE;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250607112742_Add_PrimaryKey_Role', N'9.0.5');
+
+COMMIT;
+GO
+
