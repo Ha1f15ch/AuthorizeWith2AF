fb0521b32458b2d737599a6e04a862db99df91ec
alexy.alexy98@mail.ru <alexy.alexy98@mail.ru>
2025-06-12 17:09:44 +0300

    Добавочный коммит для регистрации скриптов миграций

---
+++ AuthorizationProject/DatabaseEngine/Migrations/20250612140237_Add_VerificationCode_Table.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_VerificationCode_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserPassword",
+                schema: "dbo",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                schema: "dbo",
+                table: "RefreshToken",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "VerificationCode",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    UserEmail = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationCode", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VerificationCode_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "dbo",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationCode_UserId",
+                schema: "dbo",
+                table: "VerificationCode",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerificationCode",
+                schema: "dbo");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                schema: "dbo",
+                table: "RefreshToken");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserPassword",
+                schema: "dbo",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ AuthorizationProject/Patches/Patch_20250612_Add_VerificationCode_Table.sql
@@ -0,0 +1,31 @@
+﻿BEGIN TRANSACTION;
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[dbo].[User]') AND [c].[name] = N'UserPassword');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [dbo].[User] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [dbo].[User] ALTER COLUMN [UserPassword] nvarchar(max) NULL;
+
+ALTER TABLE [dbo].[RefreshToken] ADD [DateCreated] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+
+CREATE TABLE [dbo].[VerificationCode] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [Code] nvarchar(8) NOT NULL,
+    [UserEmail] nvarchar(100) NOT NULL,
+    [CreatedDate] datetime2 NOT NULL,
+    [ExpirationDate] datetime2 NOT NULL,
+    [IsUsed] bit NOT NULL,
+    CONSTRAINT [PK_VerificationCode] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_VerificationCode_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_VerificationCode_UserId] ON [dbo].[VerificationCode] ([UserId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250612140237_Add_VerificationCode_Table', N'9.0.5');
+
+COMMIT;
+GO
+
